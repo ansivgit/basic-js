82c451a2861c116bdc01acda9303af9fd3146c57
AnnaZAS <ansiv@yandex.ru>
2020-09-15 15:46:42 +0300

    add: task_04 what season WO throw error check

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) { return 'Unable to determine the time of year!' };
+
+  try {
+    const month = date.getMonth();
+    if (month === 0 || month === 1 || month === 11) {
+      return 'winter';
+    } else if (month >= 2 && month <= 4) {
+      return 'spring';
+    } else if (month >= 5 && month <= 7) {
+      return 'summer';
+    } else if (month) {
+      return 'autumn';
+    } else if (!date.getMonth()) { // начиная с этого места не работает как надож
+      throw new Error('THROWN');
+    }
+  } catch (e) {
+    console.log("Date Error: " + e.message);
+  }
 };
