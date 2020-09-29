b9f57ca126191a412a43f5223e5d9c21455c294d
Anna ZAS <ansiv@yandex.ru>
2020-09-29 21:49:41 +0300

    fix: add some solution for several test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  let resArr = [];
+  let arrCopy = arr.slice();
+
+  arrCopy.forEach((item, index) => {
+    //console.log(index, item === "--discard-next", index !== arr.length, typeof arr[index + 1] !== "string");
+    if (typeof(item) === "number") {
+      resArr.push(item);
+    } else if (item === "--discard-next" && index !== arrCopy.length && typeof(arrCopy[index + 1]) !== "string") {
+      arrCopy.splice(index + 1, 1);
+      //console.log(arrCopy[index + 1]);
+    } else if (item === "--discard-prev" && index !== 0 && typeof(arrCopy[index - 1]) !== "string") {
+      resArr.pop();
+    } else if (
+      item === "--double-next" && index !== arrCopy.length && typeof(arrCopy[index + 1]) !== "string") {
+      resArr.push(arrCopy[index + 1]);
+    } else if (
+      item === "--double-prev" &&  index !== 0 && typeof(arrCopy[index - 1]) !== "string" ) {
+      resArr.push(arrCopy[index - 1]);
+    }
+  });
+  return resArr;
 };
+//console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
